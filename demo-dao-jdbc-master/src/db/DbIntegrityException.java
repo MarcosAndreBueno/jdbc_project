6325bf80e6994679242741b78a4ab65466f20192
Marcos Bueno <105759509+MarcosAndreBueno@users.noreply.github.com>
2022-11-09 11:39:42 -0300

    Update DbIntegrityException.java

--- demo-dao-jdbc-master/src/db/DbIntegrityException.java
+++ demo-dao-jdbc-master/src/db/DbIntegrityException.java
@@ -1,5 +1,7 @@
 package db;
 
+// exceção especial para delete
+// lançada quando tentamos excluir uma linha com chave primária
 public class DbIntegrityException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
