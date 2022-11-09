35fc159e90402363a27f9f5c733fa16297433e76
Marcos Bueno <105759509+MarcosAndreBueno@users.noreply.github.com>
2022-11-09 12:38:31 -0300

    Update DaoFactory.java

--- demo-dao-jdbc-master/src/model/dao/DaoFactory.java
+++ demo-dao-jdbc-master/src/model/dao/DaoFactory.java
@@ -6,6 +6,9 @@ import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
+	//esse método retorna o tipo da interface, mas quem chamá-lo terá a implementação da interface (upcasting)
+	//essa é uma maneira de injetar dependência sem expôr a implementação do método (SellerDaoJDBC)
+	//e permitir que a classe que chamou este método trabalhe com um objeto mais genérico (SellerDao)
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
 	}
